4110b1951463353203131fbcdfcaff58dc93edae
modengann <99417989+modengann@users.noreply.github.com>
2022-11-01 10:39:14 -0400

    init

---
+++ src/App.java
@@ -0,0 +1,29 @@
+/** Instructions:
+*   Complete the method middle(). It should work as follow:
+*   Given a string of odd length, return the string length 3 from its middle, so "Candy" yields "and". 
+*   You can assume the given string length will have an odd number of characters and will have a length of at least 3.
+*
+*   A+ bonus challenge: 
+*   Add additional features to the code to demonstrate mastery. Be creative! Show off what you know! 
+*   This can be in an additional method or within the main. Use comments to explain what you did!
+*/
+
+public class App {
+
+    //All your work should be in the method middle(). The main method will just be for testing. Sample tests are commented out
+    //Remove the comments in the main method to run a few tests on your code, or make your own tests.
+    public static void main(String[] args) {
+       
+        //Below you can find some samples of how this method should work. Uncomment to test your code
+        //System.out.println("With an input of 'sandy', the output should be: and. Your output: " + middle("sandy"));
+        //System.out.println("With an input of 'and', the output should be: and. Your output: " + middle("and"));
+        //System.out.println("With an input of 'solving', the output should be: lvi. Your output: " + middle("solving"));
+    }
+
+    public static String middle(String str){
+        //complete your code here
+        return "";
+        
+    }
+
+}
